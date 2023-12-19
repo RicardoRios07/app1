ddb600458b63d83e275ae376e942e5149eb5228a
RicardoRios <riosr3060@gmail.com>
2023-12-18 23:23:12 -0500

    Commit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,81 @@
-import 'package:flutter/material.dart';
+class Vehicle {
+  String model;
+  int capacity;
+  String engine;
+
+  // Constructor por defecto que inicializa todas las variables
+  Vehicle() : model = '', capacity = 0, engine = '';
+
+  // Constructor por nombre para inicializar desde el motor
+  Vehicle.fromEngine(String engine) : model = '', capacity = 0, this.engine = engine;
+
+  // Constructor por nombre para inicializar con capacidad
+  Vehicle.withCapacity(int capacity) : model = '', this.capacity = capacity, engine = '';
+
+  // Otro constructor por nombre para una inicialización personalizada
+  Vehicle.custom(this.model, this.capacity, this.engine);
+
+  // Método para imprimir detalles del vehículo
+  void displayDetails() {
+    print('Model: $model, Capacity: $capacity, Engine: $engine');
+  }
+}
+
+// Clase con Constructor de Fábrica (Patrón Singleton)
+class DBConnection {
+  // Instancia única de la clase
+  static DBConnection? _instance;
+
+  // Atributos necesarios para la conexión
+  String _host;
+  int _port;
+
+  // Constructor privado para ser llamado solo desde dentro de la clase
+  DBConnection._(this._host, this._port);
+
+  // Constructor de fábrica para obtener la única instancia
+  factory DBConnection(String host, int port) {
+    // Si la instancia no existe, crear una nueva
+    _instance ??= DBConnection._(host, port);
+    return _instance!;
+  }
+
+  // Método para obtener la instancia actual
+  static DBConnection get instance {
+    // Si la instancia aún no ha sido creada, lanzar una excepción
+    assert(_instance != null, 'DBConnection not initialized. Call the factory constructor first.');
+    return _instance!;
+  }
+
+  // Otros métodos para operaciones de la base de datos
+  void connect() {
+    print('Connected to $_host:$_port');
+  }
+}
 
 void main() {
-  runApp(const MyApp());
-}
+  // Ejemplo de la clase Vehicle
+  var car = Vehicle();  // Constructor por defecto
+  car.displayDetails();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  var electricCar = Vehicle.fromEngine('Electrico');  // Constructor por parámetros
+  electricCar.displayDetails();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  var van = Vehicle.withCapacity(8);  // Constructor por nombre
+  van.displayDetails();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  var customVehicle = Vehicle.custom('SUV', 5, 'Gasolina');  // Constructor por nombre
+  customVehicle.displayDetails();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // Ejemplo de la clase DBConnection
+  var dbConnection1 = DBConnection('localhost', 3306);  // Usando el constructor de fábrica
+  dbConnection1.connect();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // Intentar crear una segunda instancia debería devolver la misma instancia
+  var dbConnection2 = DBConnection('google.com', 5432);
+  dbConnection2.connect();
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  // Usar la instancia singleton
+  var currentConnection = DBConnection.instance;
+  currentConnection.connect();
 }
